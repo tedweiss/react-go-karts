8c51dd826e36f9b6707829d12c100b845cb0180a
Ted Weiss <tedweiss@Teds-MacBook-Pro.local>
2019-07-31 19:29:43 -0400

    add inputs and labels to Info page

--- src/Info.jsx
+++ src/Info.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Info = () => {
-  return <div />
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [dob, setDob] = useState('')
+
+  return (
+    <>
+      <label>First Name</label>
+      <input
+        value={firstName}
+        onChange={e => {
+          setFirstName(e.target.value)
+        }}
+        type={'text'}
+      />
+      <label>Last Name</label>
+      <input
+        value={lastName}
+        onChange={e => {
+          setLastName(e.target.value)
+        }}
+        type={'text'}
+      />
+      <label>Email</label>
+      <input
+        value={email}
+        onChange={e => {
+          setEmail(e.target.value)
+        }}
+      />
+      <label>Date of Birth</label>
+      <input
+        value={dob}
+        onChange={e => {
+          setDob(e.target.value)
+        }}
+        type={'date'}
+      />
+    </>
+  )
 }
 
 export default Info
